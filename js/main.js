a81393a997cfbd067cf990d56fd42584588d234b
ponkotuy <web@ponkotuy.com>
2018-06-30 04:34:29 +0900

    Fix design

--- js/main.js
+++ js/main.js
@@ -1,4 +1,28 @@
-s = () => {
-  const query = document.getElementById('query').value;
-  location.href = `https://www.google.com/search?q=${query}`;
+window.onload = () => {
+  new Vue({
+    el: '#searcher',
+    data: {
+      query: ''
+    },
+    methods: {
+      search: function () {
+        const json = JSON.parse(localStorage.getItem('json'));
+        const addQuery = encodeURIComponent(json.map( elem => `-${elem.key}:${elem.value}` ).join(' '));
+        location.href = `https://www.google.com/search?q=${this.query}+${addQuery}`
+      }
+    }
+  });
+
+  const init = localStorage.getItem('json') || '[{"key": "site", "value": "example.com"}]';
+  new Vue({
+    el: '#loader',
+    data: {
+      json: init
+    },
+    methods: {
+      load: function () {
+        localStorage.setItem('json', this.json);
+      }
+    }
+  })
 };
